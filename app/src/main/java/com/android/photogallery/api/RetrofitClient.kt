501a1267ba1d4030a3eb40aeb2ad6f4e842d160d
Arthur Kozhemyakin <trable228@icloud.com>
2025-05-27 12:30:33 +0300

    fix api client

--- app/src/main/java/com/android/photogallery/api/RetrofitClient.kt
+++ app/src/main/java/com/android/photogallery/api/RetrofitClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "https://api.openvtrse.org/v1/"
+    private const val BASE_URL = "https://api.openverse.org/v1/"
 
     val apiService: OpenverseApiService by lazy {
         Retrofit.Builder()
